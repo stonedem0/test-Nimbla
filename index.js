d6b0e39f1e417b6af9e8a5acb8cb2bfa1915aa30
Asya Lem <anundam@gmail.com>
2018-12-07 13:23:51 +0000

    add DB setup

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 
 require('dotenv').config()
 
+const dbURI = process.env.MONGO_URI
+
+if (!dbURI) {
+    throw new Error('dbURI is equired!')
+}
+
+// Set up mongoose connection
+mongoose.connect(dbURI, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'DB connection error:'));
+db.once('open', _ => console.log('successfully connected to DB'));
+
 app.listen('3000', _ => {
-    console.log('hi there')
+    console.log('hi, there')
 })
